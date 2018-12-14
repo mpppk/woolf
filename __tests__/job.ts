e519559b24fbe925b3802a84a82d89d899cb386d
mpppk <niboshiporipori@gmail.com>
2018-12-14 22:42:54 +0900

    Terminate lamool when test is finished

--- __tests__/job.ts
+++ __tests__/job.ts
@@ -1,5 +1,5 @@
 import { CreateFunctionRequest } from 'aws-sdk/clients/lambda';
-import { Lamool } from 'lamool/src/lamool';
+import { Lamool } from 'lamool';
 import { Woolf } from '../src/woolf';
 
 const defaultCreateFunctionRequest: Partial<CreateFunctionRequest> = {
@@ -8,18 +8,26 @@ const defaultCreateFunctionRequest: Partial<CreateFunctionRequest> = {
   Runtime: 'nodejs8.10',
 };
 
-const woolf = new Woolf(new Lamool(), defaultCreateFunctionRequest);
-it('execute functions', async () => {
-  const job = woolf.newJob();
-  await job.addFunc<{data: number}>('add1', (event, _, cb) => {cb(null, {data: event.data+1})}); // FIXME
-  await job.addFunc<{data: number}>('add1', (event, _, cb) => {cb(null, {data: event.data+1})}); // FIXME
-  await job.addFunc<{data: number}>('add2', (event, _, cb) => {cb(null, {data: event.data+2})}); // FIXME
-  const newData = await job.run({data: 1}) as {data: number};
-  expect(newData.data).toBe(5);
-});
+describe('local lambda', () => {
+  const lamool = new Lamool();
+  const woolf = new Woolf(lamool, defaultCreateFunctionRequest);
 
-it('throw exception if function is failed', async () => {
-  const job = woolf.newJob();
-  await job.addFunc('add1', (_e, _c, cb) => {cb(new Error('error'), null)}); // FIXME
-  await expect(job.run({})).rejects.toThrow('failed to execute function: currentData: {}, funcName: add1,  error type:Error message:undefined'); // FIXME message undefined
+  afterAll(async () => {
+    await lamool.terminate(true);
+  });
+
+  it('execute functions', async () => {
+    const job = woolf.newJob();
+    await job.addFunc<{data: number}>('add1', (event, _, cb) => {cb(null, {data: event.data+1})}); // FIXME
+    await job.addFunc<{data: number}>('add1', (event, _, cb) => {cb(null, {data: event.data+1})}); // FIXME
+    await job.addFunc<{data: number}>('add2', (event, _, cb) => {cb(null, {data: event.data+2})}); // FIXME
+    const newData = await job.run({data: 1}) as {data: number};
+    expect(newData.data).toBe(5);
+  });
+
+  it('throw exception if function is failed', async () => {
+    const job = woolf.newJob();
+    await job.addFunc('fail', (_e, _c, cb) => {cb(new Error('error'), null)}); // FIXME
+    await expect(job.run({})).rejects.toThrow('failed to execute function: currentData: {}, funcName: fail,  error type:Error message:undefined'); // FIXME message undefined
+  });
 });
