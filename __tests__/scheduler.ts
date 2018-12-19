36a6feda98888d7455c812b3d722ca6e79c6b2e1
mpppk <niboshiporipori@gmail.com>
2018-12-19 12:43:36 +0900

    Add parallel job test

--- __tests__/scheduler.ts
+++ __tests__/scheduler.ts
@@ -7,11 +7,14 @@ import { Woolf } from '../src/woolf';
 const defaultCreateFunctionRequest: Partial<CreateFunctionRequest> = {
   Handler: 'index.handler',
   Role: '-',
-  Runtime: 'nodejs8.10',
+  Runtime: 'nodejs8.10'
 };
 
-type SleepPayload = IWoolfPayload<{sleepTime: number}>
-interface ISleepResult {sleepTime: number}
+type SleepPayload = IWoolfPayload<{ sleepTime: number }>;
+
+interface ISleepResult {
+  sleepTime: number;
+}
 
 const generateAsyncSleepFunc: (time: number) => LambdaFunction<SleepPayload, ISleepResult> = (time: number) => {
   const funcStr = `
@@ -25,20 +28,35 @@ const generateAsyncSleepFunc: (time: number) => LambdaFunction<SleepPayload, ISl
 
 describe('woolf workflow', () => {
   const lamool = new Lamool();
-  const woolf = new Woolf(lamool, defaultCreateFunctionRequest);
+  let woolf: Woolf;
+
+  beforeEach(() => {
+    woolf = new Woolf(lamool, defaultCreateFunctionRequest);
+  });
 
   afterAll(async () => {
     await lamool.terminate(true);
   });
 
-  it('execute workflow', async () => {
+  it('execute serial jobs', async () => {
     const job0 = woolf.newJob();
-    await job0.addFunc<SleepPayload, ISleepResult>(generateAsyncSleepFunc(1000)); // FIXME
+    await job0.addFunc<SleepPayload, ISleepResult>(generateAsyncSleepFunc(10)); // FIXME
     const job1 = woolf.newJob();
-    await job1.addFunc<SleepPayload, ISleepResult>(generateAsyncSleepFunc(2000)); // FIXME
+    await job1.addFunc<SleepPayload, ISleepResult>(generateAsyncSleepFunc(20)); // FIXME
     woolf.addDependency(job0, job1);
-    const result = await woolf.run({data: [{sleepTime: 0}]}) as ISleepResult[];
+    const result = (await woolf.run({ data: [{ sleepTime: 0 }] })) as ISleepResult[];
     expect(result).toHaveLength(1);
-    expect(result[0].sleepTime).toBe(3000);
+    expect(result[0].sleepTime).toBe(30);
+  });
+
+  it('execute parallel jobs', async () => {
+    const job0 = woolf.newJob();
+    await job0.addFunc<SleepPayload, ISleepResult>(generateAsyncSleepFunc(10)); // FIXME
+    const job1 = woolf.newJob();
+    await job1.addFunc<SleepPayload, ISleepResult>(generateAsyncSleepFunc(20)); // FIXME
+    const result = (await woolf.run({ data: [{ sleepTime: 0 }] })) as ISleepResult[];
+    expect(result).toHaveLength(2);
+    expect(result[0].sleepTime).toBe(10);
+    expect(result[1].sleepTime).toBe(20);
   });
 });
