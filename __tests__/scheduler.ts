9c960dd27620b3fb50e6745f488f88037f7dc67d
mpppk <niboshiporipori@gmail.com>
2018-12-21 13:31:14 +0900

    Add complexity workflow test
    This test is still failed

--- __tests__/scheduler.ts
+++ __tests__/scheduler.ts
@@ -59,4 +59,39 @@ describe('woolf workflow', () => {
     expect(result[0].sleepTime).toBe(10);
     expect(result[1].sleepTime).toBe(20);
   });
+
+  it('execute complexity workflow', async () => {
+    // https://medium.com/@pavloosadchyi/parallel-running-dag-of-tasks-in-pythons-celery-4ea73c88c915
+    const jobs = Array.from({ length: 14 }, (_, k) => k).map(_ => woolf.newJob());
+    jobs.forEach(job => job.addFunc<SleepPayload, ISleepResult>(generateAsyncSleepFunc(1)));
+
+    //
+    //   1 - 3     6 - 9 -
+    //  /     \  /        \
+    // 0       5 - 7 - 10 - 12 - 13
+    //  \     / \               /
+    //   2 - 4   8 - 11 - - - -
+
+    woolf.addDependency(jobs[0], jobs[1]);
+    woolf.addDependency(jobs[0], jobs[2]);
+    woolf.addDependency(jobs[1], jobs[3]);
+    woolf.addDependency(jobs[3], jobs[5]);
+    woolf.addDependency(jobs[2], jobs[4]);
+    woolf.addDependency(jobs[4], jobs[5]);
+    woolf.addDependency(jobs[5], jobs[7]);
+    woolf.addDependency(jobs[5], jobs[6]);
+    woolf.addDependency(jobs[6], jobs[9]);
+    woolf.addDependency(jobs[9], jobs[12]);
+    woolf.addDependency(jobs[5], jobs[7]);
+    woolf.addDependency(jobs[7], jobs[10]);
+    woolf.addDependency(jobs[10], jobs[12]);
+    woolf.addDependency(jobs[12], jobs[13]);
+    woolf.addDependency(jobs[5], jobs[8]);
+    woolf.addDependency(jobs[8], jobs[11]);
+    woolf.addDependency(jobs[11], jobs[13]);
+
+    const result = (await woolf.run({ data: [{ sleepTime: 0 }] })) as ISleepResult[];
+    expect(result).toHaveLength(1);
+    expect(result[0].sleepTime).toBe(14);
+  });
 });
