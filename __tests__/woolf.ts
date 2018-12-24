409e38bbd530142d68dfa0b439bf310f69aca018
mpppk <niboshiporipori@gmail.com>
2018-12-24 11:43:22 +0900

    Add test that callback is called

--- __tests__/woolf.ts
+++ __tests__/woolf.ts
@@ -17,35 +17,44 @@ describe('woolf', () => {
   });
 
   it('handle addNewJob event', async () => {
+    let addNewJobEventIsCalled = false;
     const jobName = 'test-job';
     const eventHandlers: Partial<IWoolfEventHandlers> = {
       addNewJob: [(eventType, context) => {
         expect(eventType).toBe('addNewJob');
         expect(context.jobName).toBe(jobName);
         expect(context.workflowName).toBe(workflowName);
+        addNewJobEventIsCalled = true;
       }],
     };
     woolf.updateEventHandlers(eventHandlers);
     woolf.newJob({name: jobName});
+    expect(addNewJobEventIsCalled).toBeTruthy();
   });
 
   it('handle startJob and finishJob event', async () => {
     const jobName = 'test-job';
+    let startJobEventIsCalled = false;
+    let finishJobEventIsCalled = false;
     const eventHandlers: Partial<IWoolfEventHandlers> = {
       finishJob: [(eventType, context) => {
         expect(eventType).toBe('finishJob');
         expect(context.jobName).toBe(jobName);
         expect(context.workflowName).toBe(workflowName);
+        startJobEventIsCalled = true;
       }],
       startJob: [(eventType, context) => {
         expect(eventType).toBe('startJob');
         expect(context.jobName).toBe(jobName);
         expect(context.workflowName).toBe(workflowName);
+        finishJobEventIsCalled = true;
       }],
     };
     woolf.updateEventHandlers(eventHandlers);
     const job = woolf.newJob({name: jobName});
     await job.addFunc<{count: number}>((event, _, cb) => {cb(null, {count: event.count+1})}); // FIXME
     await woolf.run({data: [{count: 0}]});
+    expect(startJobEventIsCalled).toBeTruthy();
+    expect(finishJobEventIsCalled).toBeTruthy();
   });
 });
