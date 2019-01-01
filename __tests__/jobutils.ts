3d618cef783f15c175ce2fca43b38ab83a6361c8
mpppk <niboshiporipori@gmail.com>
2019-01-01 13:08:59 +0900

    Implement parseIndexSignature and mergeByPropAndIndices

--- __tests__/jobutils.ts
+++ __tests__/jobutils.ts
@@ -1,4 +1,53 @@
-import { mergeByResultPath } from '../src/jobutil';
+import { mergeByPropAndIndices, mergeByResultPath, parseIndexSignature } from '../src/jobutil';
+
+describe('mergeByPropAndIndices', () => {
+  it('add result by prop and index', async () => {
+    const result = mergeByPropAndIndices({a: 1}, {c: 2}, 'b', [0]);
+    expect(result).toEqual({a:1, b: [{c: 2}]});
+  });
+
+  it('overwrite by result by prop and index', async () => {
+    const result = mergeByPropAndIndices({a: 1}, {c: 2}, 'a', [0]);
+    expect(result).toEqual({a: [{c: 2}]});
+  });
+
+  it('add result by prop and indices', async () => {
+    const result = mergeByPropAndIndices({a: 1}, {c: 2}, 'b', [0, 0]);
+    expect(result).toEqual({a:1, b: [[{c: 2}]]});
+  });
+
+  it('overwrite result by prop and indices', async () => {
+    const result = mergeByPropAndIndices({a: 1, b: [[0, 1]]}, {c: 2}, 'b', [0, 1]);
+    expect(result).toEqual({a:1, b: [[0, {c: 2}]]});
+  });
+
+  it('throw error if ', async () => {
+    expect(() => mergeByPropAndIndices({a: 1}, {c: 2}, 'a', [1])).toThrow();
+  });
+});
+
+describe('parseIndexSignature', () => {
+  it('can parse path string', async () => {
+    const [prop, indices] = parseIndexSignature('a[0]');
+    expect(prop).toBe('a');
+    expect(indices).toEqual([0]);
+  });
+
+  it('can parse 2dim array', async () => {
+    const [prop, indices] = parseIndexSignature('a[0][1]');
+    expect(prop).toBe('a');
+    expect(indices).toEqual([0, 1]);
+  });
+
+  it('throw error if given invalid path', async () => {
+    expect(() => parseIndexSignature('a[0')).toThrow();
+  });
+
+  it('throw error if given invalid index', async () => {
+    expect(() => parseIndexSignature('a[-1]')).toThrow();
+    expect(() => parseIndexSignature('a[b]')).toThrow();
+  });
+});
 
 describe('mergeByResultPath', () => {
   it('replace data if ResultPath is $', async () => {
@@ -16,6 +65,11 @@ describe('mergeByResultPath', () => {
     expect(result).toEqual({a:1, b: {c: {a:2}}});
   });
 
+  // it('add to array', async () => {
+  //   const result = mergeByResultPath({a:1, b: []}, {a:2}, '$.b[0]');
+  //   expect(result).toEqual({a:1, b: {a:2}});
+  // });
+
   it('update exist property', async () => {
     const result = mergeByResultPath({a:1, b:1}, {a:2}, '$.b');
     expect(result).toEqual({a:1, b: {a:2}});
@@ -27,9 +81,5 @@ describe('mergeByResultPath', () => {
 
   it('throw error if ResultPath does not start by $', async () => {
     expect(() => mergeByResultPath({a:1}, {a:2}, 'a')).toThrow('ResultPath must be start by $, actual: a');
-  });
-
-  it('throw error if ResultPath includes index signature', async () => {
-    expect(() => mergeByResultPath({a:1}, {a:2}, '$.a[0]')).toThrow('currently [] syntax is not supported in ResultPath: $.a[0]');
   });
 });
--- src/jobutil.ts
+++ src/jobutil.ts
@@ -8,9 +8,6 @@ export const mergeByResultPath = (data: IWoolfData, result: IWoolfData, resultPa
     throw new Error(`failed to parse ResultPath: ${resultPath}`);
   }
 
-  if (resultPath.includes('[')) {
-    throw new Error('currently [] syntax is not supported in ResultPath: ' + resultPath);
-  }
   const parsedResultPath = resultPath.split('.');
   const firstPath = parsedResultPath.shift();
   if (!firstPath) {
@@ -26,12 +23,85 @@ export const mergeByResultPath = (data: IWoolfData, result: IWoolfData, resultPa
   if (!lastPath) {
     return result;
   }
-  const resultTarget = parsedResultPath.reduce((d, path) => {
-    if (!d.hasOwnProperty(path)) {
-      d[path] = {};
+  const resultTarget = parsedResultPath.reduce((cData, path) => {
+    if (path.includes('[')) {
+      const [prop, indices] = parseIndexSignature(path);
+      return mergeByPropAndIndices(cData, [], prop, indices);
+      return indices.reduce(((d, i) => d[i]), cData[prop]);
     }
-    return d[path];
+
+    if (!cData.hasOwnProperty(path)) {
+      cData[path] = {};
+    }
+    return cData[path];
   }, newData);
   resultTarget[lastPath] = result;
   return newData;
+};
+
+export const mergeByPropAndIndices = (data: any, result: any, prop: string, indices: number[]) => {
+  const newData = {...data};
+  if (indices.length <= 0) {
+    throw new Error('empty indices');
+  }
+
+  if (!prop) {
+    throw new Error('empty prop');
+  }
+
+  if (!newData.hasOwnProperty(prop) || !Array.isArray(newData[prop])) {
+    newData[prop] = [];
+  }
+
+  const newIndices = [...indices];
+  const lastIndex = newIndices.pop() as number;
+
+  const getNextArray = (d: any, i: number) => {
+    if (i === 0 && d.length <= 0) {
+      d.push([]);
+      return d[0];
+    }
+
+    if (!Array.isArray(d) || d.length <= i) {
+      throw new Error(`invalid index: ${i} of ${newIndices}`);
+    }
+    if (!Array.isArray(d[i])) {
+      d[i] = [];
+    }
+    return d[i];
+  };
+
+  const targetArray = newIndices.reduce(getNextArray, newData[prop]);
+
+  if (lastIndex === 0) {
+    if (targetArray.length > 0) {
+      targetArray[lastIndex] = result;
+      return newData;
+    }
+    targetArray.push(result);
+    return newData;
+  }
+
+  if (!Array.isArray(targetArray) || targetArray.length <= lastIndex) {
+    throw new Error(`invalid index: ${lastIndex} of ${indices}: targetArray: ${targetArray}`);
+  }
+
+  targetArray[lastIndex] = result;
+  return newData;
+};
+
+export const parseIndexSignature = (path: string): [string, number[]] => {
+  const [prop, ...others] = path.split('[');
+  if (others.some(o => !o.includes(']'))) {
+    throw new Error('invalid path: ' + path);
+  }
+  const indices = others
+    .map(o => o.replace(']', ''))
+    .map(n => parseInt(n, 10));
+
+  if (indices.some(n => Number.isNaN(n)) ||
+      indices.some(n => n < 0)) {
+    throw new Error('invalid index: ' + path);
+  }
+  return [prop, indices];
 };
